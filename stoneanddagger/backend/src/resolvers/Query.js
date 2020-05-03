e611737cbd41ccd022fc492e29b4f45283dc3346
danwventura <danwventura@gmail.com>
2020-05-03 11:12:46 -0500

    Re-adding backend Query code for getting all users

--- stoneanddagger/backend/src/resolvers/Query.js
+++ stoneanddagger/backend/src/resolvers/Query.js
@@ -12,7 +12,20 @@ me(parent, args, ctx, info) {
     return ctx.db.query.user({
         where: { id: ctx.request.userId },
     }, info)
-}
+},
+async users(parent, args, ctx, info) {
+    // 1. Check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in!');
+    }
+    console.log(ctx.request.userId);
+    // 2. Check if the user has the permissions to query all the users
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+
+    // 2. if they do, query all the users!
+    return ctx.db.query.users({}, info);
+  },
+};
 };
 
 module.exports = Query;
