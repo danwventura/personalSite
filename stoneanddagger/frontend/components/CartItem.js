e5351419e4bdf4271b49552a3db224adb57d40e9
danwventura <danwventura@gmail.com>
2020-05-10 00:29:03 -0500

    frontend for removing item from cart

--- stoneanddagger/frontend/components/CartItem.js
+++ stoneanddagger/frontend/components/CartItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import formatMoney from '../lib/formatMoney';
+import RemoveFromCart from './RemoveFromCart';
 
 const CartItemStyles = styled.li`
     padding: 1rem 0;
@@ -24,6 +25,7 @@ const CartItem = ({cartItem}) => (
             <h3>{cartItem.item.title}</h3>
             <p>{formatMoney(cartItem.item.price * cartItem.quantity)} {' - '} <em>{cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each</em> </p>
         </div>
+        <RemoveFromCart id={cartItem.id}></RemoveFromCart>
     </CartItemStyles>
 )
 
